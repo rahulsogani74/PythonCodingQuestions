8aab23da4fd84828ad924d19fbb89c4464a76c86
rahulsogani74 <rahulsogani74@gmail.com>
2024-05-14 12:26:48 +0530

    Sum Of Two Number

---
+++ SumOfTwoNumbers.py
@@ -0,0 +1,15 @@
+""" 
+This script prompts the user to input two numbers, calculates their sum, and then prints the result.
+"""
+
+# Take user input for the first number
+num1 = int(input("Enter your first Number: "))
+
+# Take user input for the second number
+num2 = int(input("Enter your Second Number: "))
+
+# Calculate the sum of the two numbers
+num3 = num1 + num2
+
+# Print the sum of the numbers
+print("Sum of Your Numbers:", num3)
